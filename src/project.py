5ce5d6ce95202464e21ebcdc2e8d89befbb50060
mhoseinj <mhmtsze@gmail.com>
2026-09-01 11:58:58 +0330

    feat(render): add UV offset and scale uniforms to vertex shader for sprite sheet support

--- src/project.py
+++ src/project.py
@@ -24,10 +24,12 @@ out vec2 TexCoord;
 
 uniform mat4 model;
 uniform mat4 projection;
+uniform vec2 uvOffset;
+uniform vec2 uvScale;
 
 void main() {
     gl_Position = projection * model * vec4(aPos, 0.0, 1.0);
-    TexCoord = aTexCoord;
+    TexCoord = aTexCoord * uvScale + uvOffset;
 }
 """
 
